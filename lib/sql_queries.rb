c938927e3425cb9c308c148e5bf8c0223d77ff62
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-09 15:51:53 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -4,7 +4,7 @@ end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "SELECT names FROM bears
-  ORDER BY bears.name"
+  ORDER BY bears.name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
